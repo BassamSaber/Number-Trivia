a1d87f2b2fbd379a2113f80e0eecca75f4f05f32
Bassam Saber <bsaber@panarab-media.com>
2024-10-08 15:00:57 +0300

    - add 'get_random_number_trivia' useCase & its unit testing

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,15 +1,26 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:number_trivia/core/error/failure.dart';
+import 'package:number_trivia/core/usecases/usecase.dart';
 import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 
 import '../repositories/number_trivia_repository.dart';
 
-class GetConcreteNumberTrivia {
+class GetConcreteNumberTrivia implements UseCase<NumberTrivia, Param> {
   final NumberTriviaRepository repository;
 
   GetConcreteNumberTrivia(this.repository);
 
-  Future<Either<Failure, NumberTrivia>> call({required int number}) async {
-    return await repository.getConcreteNumberTrivia(number);
+  Future<Either<Failure, NumberTrivia>> call(Param param) async {
+    return await repository.getConcreteNumberTrivia(param.number);
   }
+}
+
+class Param extends Equatable {
+  final int number;
+
+  const Param({required this.number});
+
+  @override
+  List<Object> get props => [this.number];
 }
